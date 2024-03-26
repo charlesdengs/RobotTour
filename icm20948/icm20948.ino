9d669fb1d59611f74321843ea36031d0d17adb07
Charles <charlesdengs@gmail.com>
2024-03-25 21:27:18 -0400

    Update icm20948.ino

--- icm20948/icm20948.ino
+++ icm20948/icm20948.ino
@@ -34,8 +34,8 @@ void loop() {
 
   if (myICM.dataReady()) {
     myICM.getAGMT();           // The values are only updated when you call 'getAGMT'
-    printRawAGMT(myICM.agmt);  // Uncomment this to see the raw values, taken directly from the agmt structure
-    //printScaledAGMT(&myICM); // This function takes into account the scale settings from when the measurement was made to calculate the values with units
+    //printRawAGMT(myICM.agmt);  // Uncomment this to see the raw values, taken directly from the agmt structure
+    printScaledAGMT(&myICM); // This function takes into account the scale settings from when the measurement was made to calculate the values with units
     //delay(30);
   } else {
     SERIAL_PORT.println("Waiting for data");
